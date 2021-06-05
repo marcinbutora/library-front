b4816289691466fc8ae773376191a6f2b57b047f
mb83pl <Doctype1983!>
2021-06-05 12:06:40 +0200

    building layout, counting rows, bootstrap badges

--- library-front/src/app/app.component.ts
+++ library-front/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './interface/book';
+import { Person } from './interface/person';
+import { PersonDataService } from './service/person-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'library-front';
+
+  constructor(private pds: PersonDataService, private router: Router) {}
+
+  person: Person[] | undefined;
+  book: Book[] | undefined;
+
+  ngOnInit() {
+    this.pds.getPersonList().subscribe(data => this.person = data);
+    this.pds.getBookList().subscribe(data => this.book = data);
+  }
+
+  readersClick = () => this.router.navigateByUrl('/readers');
+  booksClick = () => this.router.navigateByUrl('/books');
+
 }
+
+
