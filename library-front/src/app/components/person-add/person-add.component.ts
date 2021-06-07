658fef19d21e3c86ac069b2863d20fa4aaa17678
mb83pl <Doctype1983!>
2021-06-07 15:05:56 +0200

    date format

--- library-front/src/app/components/person-add/person-add.component.ts
+++ library-front/src/app/components/person-add/person-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Person } from 'src/app/interface/person';
+import { DatePipe } from '@angular/common';
 import { PersonDataService } from 'src/app/service/person-data.service';
 
 @Component({
@@ -12,6 +13,7 @@ import { PersonDataService } from 'src/app/service/person-data.service';
 export class PersonAddComponent implements OnInit {
   person!: Person;
   submitted = false;
+  created = new Date();
 
   getBack() {
     this.router.navigate(['/readers']);
@@ -28,11 +30,13 @@ export class PersonAddComponent implements OnInit {
       Validators.minLength(3),
     ]),
     city: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    created: new FormControl(this.created, Validators.required)
   });
 
   constructor(private router: Router, private pds: PersonDataService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
   savePerson() {
     const person: Person = this.personAddForm.value;
     this.pds.createPerson(person).subscribe(() => {
