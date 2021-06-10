fe7a2dbb71d7c21d4bfb25d3ded68706d2f9c897
mb83pl <Doctype1983!>
2021-06-10 09:36:48 +0200

    RentalAdd Form - null in database error

--- library-front/src/app/components/rental-add/rental-add.component.ts
+++ library-front/src/app/components/rental-add/rental-add.component.ts
@@ -1,23 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { Book, Person } from 'src/app/interface/rental';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Book, Person, Rental } from 'src/app/interface/rental';
 import { BookDataService } from 'src/app/service/book-data.service';
 import { PersonDataService } from 'src/app/service/person-data.service';
+import { RentalService } from 'src/app/service/rental.service';
 
 @Component({
   selector: 'app-rental-add',
   templateUrl: './rental-add.component.html',
-  styleUrls: ['./rental-add.component.scss']
+  styleUrls: ['./rental-add.component.scss'],
 })
 export class RentalAddComponent implements OnInit {
-
   person!: Person[];
   book!: Book[];
+  created = new Date();
+  submitted = false;
 
-  constructor(private pds: PersonDataService, private bds: BookDataService) { }
+  getBack() {
+    this.router.navigate(['/readers']);
+  }
+
+  rentalAddForm = new FormGroup({
+    id: new FormControl(''),
+    book_id: new FormControl(''),
+    person_id: new FormControl(''),
+    rented_date: new FormControl(this.created, Validators.required),
+  });
+
+  constructor(
+    private pds: PersonDataService,
+    private bds: BookDataService,
+    private rds: RentalService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.pds.getPersonList().subscribe((data) => (this.person = data));
     this.bds.getBookList().subscribe((data) => (this.book = data));
   }
 
+  createRental() {
+    const rental: Rental = this.rentalAddForm.value;
+    this.rds.createRental(this.rentalAddForm.value).subscribe(() => {
+      // refreshing the list
+      this.rds.getRentalList();
+    });
+
+    this.rentalAddForm.reset();
+    this.submitted = true;
+  }
 }
