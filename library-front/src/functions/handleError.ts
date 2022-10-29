b1f586d7129be37e7cbf95c42d1d2cb1dd8a1765
mb83pl <mbutora@gmail.com>
2022-10-29 15:46:34 +0200

    commit

--- library-front/src/functions/handleError.ts
+++ library-front/src/functions/handleError.ts
@@ -3,17 +3,11 @@ import { throwError } from "rxjs";
 
 export function handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.log('An error occurred:', error.error);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
       console.log(
         `Backend returned code ${error.status}, ` + `body was: ${error.message}`
       );
     }
-    // Return an observable with a user-facing error message.
-    return throwError(`${error.message}\n${error.status}`);  
-    console.log(error.status);
-  }
-  
+    return throwError(`${error.message}\n${error.status}`);
+}
